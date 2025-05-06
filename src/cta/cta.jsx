1dcdbfa779f737928aebb690096d77a6b1177f1e
Fahad <fahadbinqaiser3@gmail.com>
2025-05-06 13:19:32 +0500

    Added padding

--- src/cta/cta.jsx
+++ src/cta/cta.jsx
@@ -1,7 +1,8 @@
 const Cta = () => {
     return(
         <>
-           <div className="max-w-5xl font-['Space_Grotesk'] mt-20 h-64 bg-gray-100 rounded-xl flex justify-between flex-col mx-6 sm:mx-auto sm:flex-row ">
+         <div className="px-20">
+         <div className="max-w-5xl font-['Space_Grotesk'] mt-20 h-64 bg-gray-100 rounded-xl flex justify-between flex-col mx-6 sm:mx-auto sm:flex-row ">
                 <div className="leftSide space-y-2 flex flex-col justify-center items-start p-10">
                     <h1 className="font-semibold text-2xl">Let's make things happen</h1>
                     <p className="text-balance">Contact us today to learn more about how our digital marketing services can help your business grow and <br /> succeed online.</p>
@@ -14,6 +15,7 @@ const Cta = () => {
                     <img src="src/assets/Illustration.png" alt="illustration" className="w-full" />
                 </div>
             </div> 
+         </div>
         </>
     )
 }
