c4cb5c238c508b3ee7d3ab5d3af39b2dbebf2c8e
Fahad <fahadbinqaiser3@gmail.com>
2025-07-13 16:30:22 +0500

    Added space grotesk font

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
     return(
         <>
-            <div className="navbar pt-10 max-w-5xl mx-auto flex justify-between items-center p-4">
+            <div className="navbar space-grotesk pt-10 max-w-5xl mx-auto flex justify-between items-center p-4">
                 <div className="leftSide flex gap-3 justify-center items-center">
                     <img src="/Star.png" className="w-9 h-w-9 object-cover" alt="Positivus Festival Logo" />
                     <h1 className="text-3xl font-semibold">Positivus</h1>
