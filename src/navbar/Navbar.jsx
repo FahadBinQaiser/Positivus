573262f37c158c536a376b08b1b0db78ab14716b
Fahad <fahadbinqaiser3@gmail.com>
2025-07-13 16:21:31 +0500

    Navbar half completed

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Navbar = () => {
     return(
         <>
-            
+            <div className="navbar max-w-5xl mx-auto flex justify-between items-center p-4">
+                <div className="leftSide flex gap-3 justify-center items-center">
+                    <img src="/Star.png" className="w-9 h-w-9 object-cover" alt="Positivus Festival Logo" />
+                    <h1 className="text-3xl font-semibold">Positivus</h1>
+                </div>
+            </div>
         </>
     )
 }
