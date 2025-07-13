0b15505d2087283348e114c29199840597698089
Fahad <fahadbinqaiser3@gmail.com>
2025-07-13 16:16:57 +0500

    navbar file created and import done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import CaseStudy from './caseStudy/case_study';
 import Cta from './cta/cta';
+import Navbar from './navbar/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Cta />
       <CaseStudy />
     </>
