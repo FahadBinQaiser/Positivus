ab5a5b9b239b4b17bb1fbdbeb639e6cb17d5498e
Fahad <fahadbinqaiser3@gmail.com>
2025-07-13 19:49:12 +0500

    Logosection done

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
       <Navbar />
-      <LogoSection />
       <HeroSection />
+      <LogoSection />
       <Cta />
       <CaseStudy />
     </>
--- src/logoSection/LogoSection.jsx
+++ src/logoSection/LogoSection.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const LogoSection = () => {
     return(
         <>
-            
+           <div className="max-w-5xl mx-auto px-8 sm:px-0">
+            <div className="flex flex-wrap justify-center md:justify-between gap-8 items-center">
+                <img src="/logos/Amazon.png" alt="Amazon" className="h-6" />
+                <img src="/logos/dribble.png" alt="Dribbble" className="h-6" />
+                <img src="/logos/Hubspot.png" alt="HubSpot" className="h-6" />
+                <img src="/logos/notion.png" alt="Notion" className="h-6" />
+                <img src="/logos/Netflix.png" alt="Netflix" className="h-6" />
+                <img src="/logos/zoom.png" alt="Zoom" className="h-6" />
+            </div>
+         </div> 
         </>
     )
 }
