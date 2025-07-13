03ecd13a7a70c6a1431d1435a77fe7822f8d00f3
Fahad <fahadbinqaiser3@gmail.com>
2025-07-13 19:43:33 +0500

    imported logosection into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import CaseStudy from './caseStudy/case_study';
 import Cta from './cta/cta';
 import Navbar from './navbar/Navbar';
 import HeroSection from './heroSection/HeroSection';
+import LogoSection from './logoSection/LogoSection'; 
 
 function App() {
   return (
     <>
       <Navbar />
+      <LogoSection />
       <HeroSection />
       <Cta />
       <CaseStudy />
